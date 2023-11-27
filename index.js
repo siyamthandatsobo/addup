e82ec7ab9b0941e06449d626db4e7461f26d3b8b
Siyamthanda Tsobo <siyamthandatsobo@gmail.com>
2023-11-27 11:25:41 +0200

    modified text

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-let btnAdd = document.querySelector('[data-addup]')
+let btnAdding = document.querySelector('[data-addup]')
 let display = document.getElementById('output')
 
-let array = [1,6,5,6]
 
 
 
-btnAdd.addEventListener('click',function(){
+
+let array = [1,2,8,4,6,7,8,9,7,5]
+
+
+
+
+
+btnAdding.addEventListener('click',function(){
     sumofnum(array)
 })
 function sumofnum(arr) {
    let sum = 0
    let hasNumbers = false;
+
+
+
+
    
    for(let i = 0;i<arr.length;i++){
     if(typeof arr[i] === 'number'){
         sum += arr[i]
         hasNumbers= true;
     }
+
+
+
+
+
+    
    }
 
    if(hasNumbers){
